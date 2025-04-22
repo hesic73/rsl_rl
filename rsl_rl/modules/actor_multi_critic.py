fix advantage normalization bug; some minor change

rsl_rl/modules/actor_multi_critic.py
@@ -94,15 +94,15 @@ class ActorMultiCritic(nn.Module):
         raise NotImplementedError
 
     @property
-    def action_mean(self):
+    def action_mean(self) -> torch.Tensor:
         return self.distribution.mean
 
     @property
-    def action_std(self):
+    def action_std(self) -> torch.Tensor:
         return self.distribution.stddev
 
     @property
-    def entropy(self):
+    def entropy(self) -> torch.Tensor:
         return self.distribution.entropy().sum(dim=-1)
 
     def update_distribution(self, observations: torch.Tensor):
@@ -122,10 +122,10 @@ class ActorMultiCritic(nn.Module):
         self.update_distribution(observations)
         return self.distribution.sample()
 
-    def get_actions_log_prob(self, actions):
+    def get_actions_log_prob(self, actions: torch.Tensor) -> torch.Tensor:
         return self.distribution.log_prob(actions).sum(dim=-1)
 
-    def act_inference(self, observations: torch.Tensor):
+    def act_inference(self, observations: torch.Tensor) -> torch.Tensor:
         actions_mean = self.actor(observations)
         return actions_mean
 

rsl_rl/runners/on_policy_runner.py
@@ -202,6 +202,10 @@ class OnPolicyRunner:
                     actions = self.alg.act(obs, privileged_obs)
                     # Step the environment
                     obs, rewards, dones, infos = self.env.step(actions.to(self.env.device))
+                    # NOTE (hsc): 允许rewards是多维的，这样_get_rewards不需要区分Single Critic和Multi Critic
+                    if len(rewards.shape) != 1:
+                        assert len(rewards.shape) == 2
+                        rewards = rewards.sum(dim=1)
                     # Move to device
                     obs, rewards, dones = (obs.to(self.device), rewards.to(self.device), dones.to(self.device))
                     # perform normalization
@@ -455,7 +459,8 @@ class OnPolicyRunner:
         if self.cfg["empirical_normalization"]:
             if device is not None:
                 self.obs_normalizer.to(device)
-            policy = lambda x: self.alg.policy.act_inference(self.obs_normalizer(x))  # noqa: E731
+
+            def policy(x): return self.alg.policy.act_inference(self.obs_normalizer(x))  # noqa: E731
         return policy
 
     def train_mode(self):

rsl_rl/storage/multi_critic_rollout_storage.py
@@ -104,20 +104,22 @@ class MultiCriticRolloutStorage:
             else:
                 next_values = self.values[step + 1]
             # 1 if we are not in a terminal state, 0 otherwise
-            next_is_not_terminal = 1.0 - self.dones[step].float()
+            next_is_not_terminal = 1.0 - self.dones[step].float()  # (num_envs, 1)
             # TD error: r_t + gamma * V(s_{t+1}) - V(s_t)
-            delta = self.rewards[step] + next_is_not_terminal * gamma * next_values - self.values[step]
+            delta = self.rewards[step] + next_is_not_terminal * gamma * next_values - self.values[step]  # (num_envs, num_critics)
             # Advantage: A(s_t, a_t) = delta_t + gamma * lambda * A(s_{t+1}, a_{t+1})
-            advantage = delta + next_is_not_terminal * gamma * lam * advantage
+            advantage = delta + next_is_not_terminal * gamma * lam * advantage  # (num_envs, num_critics)
             # Return: R_t = A(s_t, a_t) + V(s_t)
             self.returns[step] = advantage + self.values[step]
 
         # Compute the advantages
-        self.advantages = self.returns - self.values
+        self.advantages = self.returns - self.values  # (num_transitions_per_env, num_envs, num_critics)
         # Normalize the advantages if flag is set
         # This is to prevent double normalization (i.e. if per minibatch normalization is used)
         if normalize_advantage:
-            self.advantages = (self.advantages - self.advantages.mean(dim=0, keepdim=True)) / (self.advantages.std(dim=0, keepdim=True) + 1e-8)
+            flat_adv = self.advantages.view(-1, self.advantages.shape[-1])  # (T*N, C)
+            flat_adv = (flat_adv - flat_adv.mean(dim=0, keepdim=True)) / (flat_adv.std(dim=0, keepdim=True) + 1e-8)
+            self.advantages = flat_adv.view_as(self.advantages)
 
     # for reinforcement learning with feedforward networks
     def mini_batch_generator(self, num_mini_batches: int, num_epochs: int = 8):